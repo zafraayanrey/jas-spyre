50ed409427b7604b800e6ce96e1b4cba7599c484
zafraayanrey <zafraayanrey@gmail.com>
2024-11-06 00:37:10 +0800

    v

---
+++ src/supabase.js
@@ -0,0 +1,18 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Replace these with your own Supabase project URL and anon key
+const supabaseUrl = "https://vqdccsdntrhupjzqcupr.supabase.co"; // Your Supabase URL
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxZGNjc2RudHJodXBqenFjdXByIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MjA1MTYsImV4cCI6MjA0NjM5NjUxNn0.fqx4hKTgbHc_eRHubtl93vEbUaD65obdZXWyKKJBxGY"; // Your Supabase anon key
+
+// Initialize the Supabase client
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+async function reading() {
+  const { data, error } = await supabase.from("sales").select();
+
+  if (error) console.log("Failed Fetching the data");
+  if (data) console.log(data);
+}
+reading();
+export default supabase;
