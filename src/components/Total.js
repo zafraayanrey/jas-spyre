7591d95a830d4450fe9b630f9836c5459b255529
zafraayanrey <zafraayanrey@gmail.com>
2024-11-14 23:39:41 +0800

    v

--- src/components/Total.js
+++ src/components/Total.js
@@ -19,7 +19,7 @@ const monthText = [
 function Total() {
   const [records, setRecords] = useState([]);
   const [date, setDate] = useState([]);
-  const [expenses, setExpenses] = useState([]);
+  const [monthNumber, setMonthNumber] = useState();
   const [ayaw, setAyaw] = useState();
 
   async function salesData() {
@@ -27,48 +27,75 @@ function Total() {
     const { data, error } = await supabase.from("sales").select("*");
     if (error) console.log(error);
     // if (data) data.map((el) => dateContainer.push(el.date));
-    if (data) {
-      const mik = [];
-      for (let x = 0; x < 12; x++) {
-        const months = data
-          .filter((el) => new Date(el.date).getMonth() === x)
-          .map((el) => el.price)
-          .reduce((acc, curr) => acc + curr, 0);
-        if (months) mik.push(months);
-      }
-      setAyaw(mik);
-    }
-
-    const me = [];
-    // dateContainer.map((el, i) => me.push(el.charAt(5).concat(el.charAt(6))));
-    dateContainer.map((el, i) => me.push(new Date(el)));
-
-    // console.log(me);
-
-    // const uniqueMonths = [...new Set(me)]; // merging duplicate values
-    // me.map((el) => [
-    //   console.log(monthText[el.getMonth()]),
-    //   console.log(el.getDate()),
-    //   console.log(el.getFullYear()),
-    // ]);
-    // me.map((el) => console.log(el.getMonth()));
-    // me.map((el) => console.log(el.getFullYear()));
-    // setDate(me);
+    if (data) setRecords(data);
   }
 
-  // console.log(date);
+  function monthlyTotal() {
+    const months = records
+      .filter((el) => new Date(el.date).getMonth() === monthNumber)
+      .map((el) => el.price)
+      .reduce((acc, curr) => acc + curr, 0);
+    setAyaw(months);
+  }
 
-  // console.log(dateVariable);
   useEffect(() => {
     salesData();
-  }, []);
+    monthlyTotal();
+  }, [monthNumber]);
 
+  function selectedMonth(e) {
+    const selectMon = e.target.value;
+    switch (selectMon) {
+      case monthText[0]:
+        setMonthNumber(0);
+        break;
+      case monthText[1]:
+        setMonthNumber(1);
+        break;
+      case monthText[2]:
+        setMonthNumber(2);
+        break;
+      case monthText[3]:
+        setMonthNumber(3);
+        break;
+      case monthText[4]:
+        setMonthNumber(4);
+        break;
+      case monthText[5]:
+        setMonthNumber(5);
+        break;
+      case monthText[6]:
+        setMonthNumber(6);
+        break;
+      case monthText[7]:
+        setMonthNumber(7);
+        break;
+      case monthText[8]:
+        setMonthNumber(8);
+        break;
+      case monthText[9]:
+        setMonthNumber(9);
+        break;
+      case monthText[10]:
+        setMonthNumber(10);
+        break;
+      case monthText[11]:
+        setMonthNumber(11);
+        break;
+      default:
+        return selectMon;
+    }
+  }
+
+  // console.log(monthNumber);
   return (
     <div>
-      {date.map((el, i) => (
-        <div key={i}>{el}</div>
-      ))}
-      {/* {ayaw.map((el) => el)} */}
+      <select onChange={selectedMonth}>
+        {monthText.map((el) => (
+          <option>{el}</option>
+        ))}
+      </select>
+      {ayaw}
     </div>
   );
 }
